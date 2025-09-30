02a3aefa83338afa5df03a3427290be36eae3741
Koll VossFam <valentin2v2000@gmail,com>
2025-09-30 14:35:34 -0500

    Add CSP headers for Epic SMART on FHIR integration

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,23 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://*.epic.com https://epic.com;",
+          },
+        ],
+      },
+    ];
+  },
   webpack: (config) => {
     config.experiments = { 
       asyncWebAssembly: true, 
